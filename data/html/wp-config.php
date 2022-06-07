9ecae00c8122c9599d5426a6f009b60621c29283
Geek <rohitchauhan916@hotmail.com>
2022-06-07 12:44:53 +0530

    super admin setup

--- data/html/wp-config.php
+++ data/html/wp-config.php
@@ -124,6 +124,15 @@ if ($configExtra = getenv_docker('WORDPRESS_CONFIG_EXTRA', '')) {
 
 /* That's all, stop editing! Happy publishing. */
 
+define('WP_ALLOW_MULTISITE', true);
+define( 'MULTISITE', true );
+define( 'SUBDOMAIN_INSTALL', false );
+define( 'DOMAIN_CURRENT_SITE', 'localhost:8080' );
+define( 'PATH_CURRENT_SITE', '/' );
+define( 'SITE_ID_CURRENT_SITE', 1 );
+define( 'BLOG_ID_CURRENT_SITE', 1 );
+
+
 /** Absolute path to the WordPress directory. */
 if ( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', __DIR__ . '/' );
